40562c4f337e79ba7269681f099ab2d8b42f5e59
Oussema Haggui <medoussemahaggui@gmail.com>
2025-02-14 11:37:37 +0100

    controle de saisie

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -15,6 +15,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\File;
 
 class RegistrationFormType extends AbstractType
@@ -25,19 +27,28 @@ class RegistrationFormType extends AbstractType
             ->add('nom', TextType::class, [
                 'attr' => ['class' => 'form-control'],
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter your last name.']),
+                    new NotBlank(['message' => 'Veuillez entrer votre nom de famille.']),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z\s]+$/',
+                        'message' => 'Votre nom de famille ne doit contenir que des lettres et des espaces.',
+                    ]),
                 ],
             ])
             ->add('prenom', TextType::class, [
                 'attr' => ['class' => 'form-control'],
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter your first name.']),
+                    new NotBlank(['message' => 'Veuillez entrer votre prénom.']),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z\s]+$/',
+                        'message' => 'Votre prénom ne doit contenir que des lettres et des espaces.',
+                    ]),
                 ],
             ])
             ->add('email', TextType::class, [
                 'attr' => ['class' => 'form-control'],
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter your email.']),
+                    new NotBlank(['message' => 'Veuillez entrer votre adresse email.']),
+                    new Email(['message' => 'Veuillez entrer une adresse email valide.']),
                 ],
             ])
             ->add('travail', ChoiceType::class, [
@@ -48,26 +59,30 @@ class RegistrationFormType extends AbstractType
                 ],
                 'attr' => ['class' => 'form-control'],
                 'constraints' => [
-                    new NotBlank(['message' => 'Please select your role.']),
+                    new NotBlank(['message' => 'Veuillez sélectionner votre rôle.']),
                 ],
             ])
             ->add('numTel', TelType::class, [
                 'attr' => ['class' => 'form-control'],
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter your phone number.']),
-                    new Length(['min' => 8, 'max' => 15]),
+                    new NotBlank(['message' => 'Veuillez entrer votre numéro de téléphone.']),
+                    new Length(['min' => 8, 'max' => 15, 'minMessage' => 'Le numéro de téléphone doit contenir au moins {{ limit }} chiffres.', 'maxMessage' => 'Le numéro de téléphone ne doit pas dépasser {{ limit }} chiffres.']),
+                    new Regex([
+                        'pattern' => '/^[0-9]+$/',
+                        'message' => 'Le numéro de téléphone ne doit contenir que des chiffres.',
+                    ]),
                 ],
             ])
             ->add('photoUrl', FileType::class, [
-                'label' => 'Profile Photo',
-                'mapped' => false, // This field is not mapped to the entity
-                'required' => false, // The file is optional
+                'label' => 'Photo de profil',
+                'mapped' => false, // Ce champ n'est pas mappé à l'entité
+                'required' => false, // Le fichier est facultatif
                 'attr' => ['class' => 'form-control'],
                 'constraints' => [
                     new File([
                         'maxSize' => '1024k',
                         'mimeTypes' => ['image/jpeg', 'image/png', 'image/gif'],
-                        'mimeTypesMessage' => 'Please upload a valid image file (JPEG, PNG, GIF).',
+                        'mimeTypesMessage' => 'Veuillez télécharger un fichier image valide (JPEG, PNG, GIF).',
                     ]),
                 ],
             ])
@@ -75,14 +90,18 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password', 'class' => 'form-control'],
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter a password.']),
-                    new Length(['min' => 6, 'max' => 4096]),
+                    new NotBlank(['message' => 'Veuillez entrer un mot de passe.']),
+                    new Length(['min' => 6, 'max' => 4096, 'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères.']),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
+                        'message' => 'Le mot de passe doit contenir au moins une majuscule, une minuscule et un chiffre.',
+                    ]),
                 ],
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
-                    new IsTrue(['message' => 'You should agree to our terms.']),
+                    new IsTrue(['message' => 'Vous devez accepter nos conditions.']),
                 ],
             ]);
     }
